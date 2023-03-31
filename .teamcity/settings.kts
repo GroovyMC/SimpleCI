046d375f853af4843dad94d5ff8f16509f6a695d
Matyrobbrt <matyrobbrt@gmail.com>
2023-03-31 23:46:21 +0300

    Update TC settings again

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -10,17 +10,12 @@ The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
 project() function with a Project instance or an init function as
 an argument.
-
 VcsRoots, BuildTypes, Templates, and subprojects can be
 registered inside the project using the vcsRoot(), buildType(),
 template(), and subProject() methods respectively.
-
 To debug settings scripts in command-line, run the
-
     mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
 command and attach your debugger to the port 8000.
-
 To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 -> Tool Windows -> Maven Projects), find the generate task node
 (Plugins -> teamcity-configs -> teamcity-configs:generate), the
@@ -36,11 +31,36 @@ project {
 object GroovyMC_SimpleCI_Build : BuildType({
     id("Build")
     name = "Build"
-    templates(AbsoluteId("GroovyMC_PublishUsingGradle"))
 
-    params {
-        text("github_project_name", "MinecraftForge")
-        text("github_repository_name", "SimpleCI")
-        text("git_main_branch", "master")
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    triggers {
+        vcs {
+            triggerRules = "-:comment=\\[noci]:**"
+        }
+    }
+
+    features {
+        add {
+            swabra {
+                filesCleanup = Swabra.FilesCleanup.AFTER_BUILD
+                lockingProcesses = Swabra.LockingProcessPolicy.KILL
+            }
+        }
+    }
+
+    steps {
+        gradle {
+            name = "Configure TeamCity information"
+            tasks = "configureTeamCity"
+        }
+
+        gradle {
+            name = "Build Gradle Project"
+            tasks = "clean publish"
+            jvmArgs = "-Xmx1G"
+        }
     }
 })
